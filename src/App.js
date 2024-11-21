9936ae92d99011e517d29ccdf87a51cedb4cf13f
JhonatanNY <jhonatanset1989@gmail.com>
2024-11-20 21:29:37 -0500

    Subiendo FrontEnd 1

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+import React from 'react';
+import ListaBuses from './ListaBuses';
+
+function App() {
+
+  return (
+    <div className="App">
+      <ListaBuses /> {/* Renderiza el componente de la lista de buses */}
+    </div>
+  );
+}
+
+
+export default App;
